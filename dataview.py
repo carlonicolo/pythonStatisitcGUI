bd98b34342dbfadb9a073faacc5da9defeb740de
Carlo <carlo.nicolo@hotmail.com>
2024-01-08 01:42:50 +0100

    Adding style

--- dataview.py
+++ dataview.py
@@ -20,6 +20,40 @@ class Application(Frame):
         self.l1.grid(row=0)
         self.l2.grid(row=1)
         self.l3.grid(row=2)
+        
+        self.eFrame = Entry(self.master, width=40)
+        self.eX = Entry(self.master, width=40)
+        self.eY = Entry(self.master, width=40)
+        
+        self.eFrame.grid(row=0, column=1, sticky=W)
+        self.eX.grid(row=1, column=1, sticky=W)
+        self.eY.grid(row=2, column=1, sticky=W)
+        
+        self.txtX = Text(self.master, width=30)
+        self.txtY = Text(self.master, width=30)
+        
+        self.txtX.grid(row=3, column=0, sticky=W)
+        self.txtY.grid(row=3, column=1, sticky=W)
+        
+        self.style = Style()
+        self.style.map('D.TButton',
+                       foreground=[('pressed', 'red'), ('active', 'green')],
+                       background=[('pressed', '!disabled', 'black'), ('active', 'white')]
+                       )
+        
+        self.btn = Button(self.master, text="Show Regression Graph", style="D.TButton")
+        self.btn.grid(row=4, column=0, sticky=W)
+        
+        self.stats = Button(self.master, text="Show Stats",
+                            style="D.TButton")
+        self.stats.grid(row=4, column=1, sticky=W)
+        
+        self.quit = Button(self.master, text="Quit",
+                           style="D.TButton", command=self.master.destroy)
+        self.quit.grid(row=4, column=1, sticky=E)
+        
+        
+        
 
 
 root = Tk()
